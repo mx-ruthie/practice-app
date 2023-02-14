1cf6ce6b0e4bd9fc57dbe57c2b6a6ab78452e7e5
Ruthie Irvin <ruth.diane.irvin@gmail.com>
2023-02-14 16:38:20 -0600

    shows some stuff to point out to cohort

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (this is the placeholder text that happens in the react before you change it to your awesome stuff)
         </a>
       </header>
       
